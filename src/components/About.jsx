13a5e7a8dd0f97647dfe9641914797b5f7e98754
Alejandro <>
2024-02-03 02:34:22 -0600

    Beginning to flesh out About.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+// import personal-photo from './public/'
+
+function About() {
+  return (
+    <div id="about-container">
+      <h1>Alejandro D. Nava</h1>
+      <img src="" alt="Photo of me, Alejandro"/>
+      <h2>Studying, Coding, Learning, Applying</h2>
+      <p>
+        For many, learning Web Development may sound "easy" or "not a challenging field",
+        but that's because they only understand the tip of the iceberg of web development
+        <br/><br/>
+        I have always had a passion for web development ever since I was exposed to it in
+        my tech highschool, Lakeview Academy. I only understood html, 
+      </p>
+    </div>
+  )
+}
+
+export default About;
