842ab3c0b196c30402df43171ff8a625d4c308d0
Alejandro <>
2024-02-05 18:07:36 -0600

    Still inserting more content in About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,13 +5,52 @@ function About() {
     <div id="about-container">
       <h1>Alejandro D. Nava</h1>
       <img src="" alt="Photo of me, Alejandro"/>
+      
       <h2>Studying, Coding, Learning, Applying</h2>
       <p>
-        For many, learning Web Development may sound "easy" or "not a challenging field",
-        but that's because they only understand the tip of the iceberg of web development
+        TLDR: Highschool was when I learned HTML; I grew personal experience by coding personal
+        projects; undertook a total of two bootcamps to acquire knowledge; studied video
+        game-based coding concepts and business-based technology; acquired an Associates in GS,
+        acquired college credits for a Bachelors in Cybersecurity; formally worked as a satellite
+        tech aid; continuously learned more tools, html tricks, and methods to keep improving old
+        and dated code-work; now looking to apply these skills in a career-based route.
+      </p>
+      
+      <h2>Web Development Isn't Hard, Right?</h2>
+      <p>
+        For many, learning Web Development may sound "easy" or "not a challenging field", but
+        that's because they only understand the tip of the iceberg of web development. There's a
+        lot more to Web Development than just appearances. How was it structured? Is it easy to
+        read when something on the page doesn't work? Does the page even function properly? How
+        does it gather data? How does it process said data? Does it even secure the data being
+        inserted, sent, and received? How much time was actually spent in building up the page?
+        What makes it different than another web page? Thousands of questions to ask, but most
+        go unanswered if the developer or dev team can't provide answers with full confidence.
+      </p>
+
+      <h2>My Development Story</h2>
+      <p>
+        I had a passion for web development ever since I was exposed to it in highschool - 
+        Lakeview Technology Academy. Back then, I only understood HTML, CSS, and Java, but that
+        was enough building blocks to appreciate how webpages were crafted, appeared, and
+        interacted with users. I then got involved in 'forum code' (Proboards, JCINK, etc),
+        which was more or less a distant cousin of HTML and CSS, but allowed me to explore and
+        experiment with HTML and CSS in ways I couldn't in a classroom setting.
         <br/><br/>
-        I have always had a passion for web development ever since I was exposed to it in
-        my tech highschool, Lakeview Academy. I only understood html, 
+        From there, I got involved in learning IT in general, to see what other coding language
+        or field would stick to me. Dabbled in hardware knowledge, video game knowledge,
+        various software knowledge, attended and passed a coding bootcamp through Gateway
+        Technical College, dabbled in satellite knowledge, gained vast amounts of information
+        on Cybersecurity through online courses provided by Columbia College of Missouri, and
+        now recently attended and passed a newer coding bootcamp through Northwestern University
+        + EdX.
+        <br/><br/>
+        Could I have done most of the coding knowledge on my own through online tutorials
+        and videos? Probably, I know a handful who have done that. However, without the
+        series of paths I've taken so far, albeit long and frustrating at times, I wouldn't
+        have a different perspective that most probably don't realize when they're teaching
+        themselves, or simply go find a career based on a one-way education path. That's what
+        I'm 
       </p>
     </div>
   )
