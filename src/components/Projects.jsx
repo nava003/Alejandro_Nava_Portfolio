58531167c7b6c38790b59d393691664625703ac2
Alejandro <>
2024-02-05 22:27:09 -0600

    Beginning to flesh out Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,14 @@
 function Projects() {
   return (
-    <div>List of Projects Here</div>
+    <div id="projects-container">
+      <h1>Projects Involved</h1>
+      <a><img/></a>
+      <a><img/></a>
+      <a><img/></a>
+      <a><img/></a>
+      <a><img/></a>
+      <a><img/></a>
+    </div>
   )
 }
 
