5574380688007d6e54b75fe1e91ffd069ad0c30e
Alejandro <>
2024-02-07 03:56:39 -0600

    Fleshing out Projects structurally

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,12 +2,14 @@ function Projects() {
   return (
     <div id="projects-container">
       <h1>Projects Involved</h1>
-      <a><img/></a>
-      <a><img/></a>
-      <a><img/></a>
-      <a><img/></a>
-      <a><img/></a>
-      <a><img/></a>
+        <div className="project-cards">
+          <a href=""><img src="./public/HobbyHouseIcon.png"/></a>
+          <a href=""><img src=""/></a>
+          <a href=""><img src=""/></a>
+          <a href=""><img src=""/></a>
+          <a href=""><img src=""/></a>
+          <a href=""><img src=""/></a>
+        </div>
     </div>
   )
 }
