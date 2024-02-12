b078fc90dc60de91884b9363cd1a192a8e47fda6
Alejandro <>
2024-02-12 17:36:13 -0600

    Sourcing images into Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,12 +3,12 @@ function Projects() {
     <div id="projects-container">
       <h1>Projects Involved</h1>
         <div className="project-cards">
-          <a href=""><img src="./public/HobbyHouseIcon.png"/></a>
-          <a href=""><img src=""/></a>
-          <a href=""><img src=""/></a>
-          <a href=""><img src=""/></a>
-          <a href=""><img src=""/></a>
-          <a href=""><img src=""/></a>
+          <a href=""><img src="./public/HobbyHouseIcon.jpeg" alt="Image of Hobby_House_Icon"/></a>
+          <a href=""><img src="./public/BookSearchEngine.jpg" alt="Image of Book_Search_Engine"/></a>
+          <a href=""><img src="./public/NoteTakerApp.jpg" alt="Image of Note_Taker_App"/></a>
+          <a href=""><img src="./public/plant-leaf.svg" alt="Image of a plant"/></a>
+          <a href=""><img src="./public/WorkDayScheduler.jpg" alt="Image of a scheduler project"/></a>
+          <a href=""><img src="./public/ArtistLookup.jpg" alt="Image of an artist lookup project"/></a>
         </div>
     </div>
   )
