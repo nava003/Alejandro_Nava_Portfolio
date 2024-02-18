6c9b331865af9f712e8c56b29486e08d00606a90
Alejandro <>
2024-02-18 07:35:53 -0600

    Adding Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import About from './components/About';
 import Contact from './components/Contact';
+import Header from './components/Header';
 import Nav from './components/Nav';
 import Projects from './components/Projects';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className='portfolio-app'>
       <Nav/>
+      <Header/>
       <About/>
       <Projects/>
       <Contact/>
