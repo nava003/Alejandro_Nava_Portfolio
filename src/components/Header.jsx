bf507db7f656538b6803e5b5c11eff4cde1cd371
Alejandro <>
2024-02-21 02:22:04 -0600

    Different approach to see if matrix effect can dynamically redraw based on screen size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,33 +2,31 @@ import { useState, useEffect, useRef } from "react";
 
 function Header() {
   const canvasRef = useRef(null);
-  const [size, setSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
+  const [canvasWidth, setWidth] = useState(window.innerWidth);
+  const [canvasHeight, setHeight] = useState(window.innerHeight);
 
   // Once the component mounts, run this callback effect
   useEffect(() => {
-    const canvas = document.getElementById('brailleMatrix');
+    const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
+
+    const fontSize = 16;
+    const columns = size.width/fontSize;
+
+    const brailleUnicode = [];
+    for (let i = 0; i < 100; i++) {
+      brailleUnicode[i] = String.fromCharCode(0x2800 + Math.random() * (0x283F-0x2800+1))
+    }
+
+    const raindropEffect = [];
+    for (let i = 0; i < columns; i++) {
+      raindropEffect[i] = 1;
+    }
     
     // Draw the Matrix raindrop effect
     const drawMatrix = () => {
-      canvas.width = size.width;
-      canvas.height = size.height;
-
-      const fontSize = 16;
-      const columns = canvas.width/fontSize;
-  
-      const brailleUnicode = [];
-      for (let i = 0; i < 100; i++) {
-        brailleUnicode[i] = String.fromCharCode(0x2800 + Math.random() * (0x283F-0x2800+1))
-      }
-  
-      const raindropEffect = [];
-      for (let i = 0; i < columns; i++) {
-        raindropEffect[i] = 1;
-      }
+      canvas.width = canvasWidth;
+      canvas.height = canvasHeight;
 
       context.fillStyle = 'rgba(0, 0, 0, 0.05)';
       context.fillRect(0, 0, canvas.width, canvas.height);
@@ -52,7 +50,8 @@ function Header() {
     setInterval(drawMatrix, 30);
 
     const handleResize = () => {
-      setSize(window.innerWidth, window.innerHeight);
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
     }
 
     window.addEventListener('resize', handleResize);
@@ -60,7 +59,7 @@ function Header() {
     return () => {
       window.removeEventListener('resize', handleResize);
     }
-  }, [size.width, size.height]);
+  }, [canvasWidth, canvasHeight]);
 
   return (
     <header id="home">
