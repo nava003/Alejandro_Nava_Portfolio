afd7d647c44c380517f9c16039cda03e3d07d068
Alejandro <>
2024-02-21 02:01:29 -0600

    Continuation in dynamically redrawing Matrix Effect when screen size changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,35 @@
-import { useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function Header() {
+  const canvasRef = useRef(null);
+  const [size, setSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  // Once the component mounts, run this callback effect
   useEffect(() => {
     const canvas = document.getElementById('brailleMatrix');
     const context = canvas.getContext('2d');
-
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    const fontSize = 16;
-    const columns = canvas.width/fontSize;
-
-    const brailleUnicode = [];
-    for (let i = 0; i < 100; i++) {
-      brailleUnicode[i] = String.fromCharCode(0x2800 + Math.random() * (0x283F-0x2800+1))
-    }
-
-    const raindropEffect = [];
-    for (let i = 0; i < columns; i++) {
-      raindropEffect[i] = 1;
-    }
     
-    const draw = () => {
+    // Draw the Matrix raindrop effect
+    const drawMatrix = () => {
+      canvas.width = size.width;
+      canvas.height = size.height;
+
+      const fontSize = 16;
+      const columns = canvas.width/fontSize;
+  
+      const brailleUnicode = [];
+      for (let i = 0; i < 100; i++) {
+        brailleUnicode[i] = String.fromCharCode(0x2800 + Math.random() * (0x283F-0x2800+1))
+      }
+  
+      const raindropEffect = [];
+      for (let i = 0; i < columns; i++) {
+        raindropEffect[i] = 1;
+      }
+
       context.fillStyle = 'rgba(0, 0, 0, 0.05)';
       context.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -40,15 +48,28 @@ function Header() {
       }
     }
 
-    setInterval(draw, 30);
-  }, []);
+    // Callback the draw function at a rate of 30 milliseconds
+    setInterval(drawMatrix, 30);
+
+    const handleResize = () => {
+      setSize(window.innerWidth, window.innerHeight);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, [size.width, size.height]);
 
   return (
     <header id="home">
-      <canvas id="brailleMatrix"></canvas>
-      {/* <div className="header-container" ></div> */}
-      <h1>ALEJANDRO D<br/>NAVA</h1>
-      <img src="" alt="Photo of me, Alejandro"/>
+      <canvas id="brailleMatrix" ref={canvasRef}/>
+      <div className="header-container" >
+        <h1>ALEJANDRO D<br/>NAVA</h1>
+        <img src="" alt="Photo of me, Alejandro"/>
+      </div>
+      
     </header>
   )
 }
