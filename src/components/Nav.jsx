47dab32e11174932e18d0254a3a09c397d8dd112
Alejandro <>
2024-02-21 05:10:46 -0600

    Beginning to apply logic for home button to appear when user scrolls down on page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,13 @@
 function Nav() {
+  const homeBtn = document.getElementById('homeBtn');
+  
+
   return (
     <nav>
       <a href="#about"><img title="About" src="/person.svg" alt="SVG icon image of a person."/></a>
       <a href="#projects"><img title="Projects" src="/document.svg" alt="SVG icon image of a document."/></a>
       <a href="#contact"><img title="Contact" src="/phone.svg" alt="SVG icon image of a phone."/></a>
-      <a href="#home"><img title="Return to Top" src="/rocket.svg" alt="SVG icon image of a rocket."/></a>
+      <a href="#home" id="homeBtn"><img title="Return to Top" src="/rocket.svg" alt="SVG icon image of a rocket."/></a>
     </nav>
   )
 }
