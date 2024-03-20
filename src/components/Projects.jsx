3bfe9893983dc0a6361b3557f289de067d7b363f
Alejandro <>
2024-03-20 00:40:58 -0500

    Reformatted Projects.jsx structure; placed each project into its own div 'card' for Css styling; added temp descriptive text for future styling

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,16 +2,52 @@ function Projects() {
   return (
     <div id="projects" className="projects-container">
       <h1>Projects Involved</h1>
-        <div className="project-cards">
-          <a href=""><img src="/HobbyHouseIcon.jpeg" alt="Image of Hobby_House_Icon"/></a>
-          <a href=""><img src="/BookSearchEngine.jpg" alt="Image of Book_Search_Engine"/></a>
-          <a href=""><img src="/NoteTakerApp.jpg" alt="Image of Note_Taker_App"/></a>
-          <a href=""><img src="/plant-leaf.svg" alt="SVG icon image of a plant."/></a>
-          <a href=""><img src="/WorkDayScheduler.jpg" alt="Image of a scheduler project"/></a>
-          <a href=""><img src="/ArtistLookup.jpg" alt="Image of an artist lookup project"/></a>
+      <div id="project-cards">
+        <div className="card">
+          <a href="">
+            <img src="/HobbyHouseIcon.jpeg" alt="Image of Hobby_House_Icon" />
+          </a>
+          <h2>Hobby House</h2>
+          <p>Description</p>
         </div>
+        <div className="card">
+          <a href="">
+            <img src="/BookSearchEngine.jpg" alt="Image of Book_Search_Engine" />
+          </a>
+          <h2>Book Search</h2>
+          <p>Description</p>
+        </div>
+        <div className="card">
+          <a href="">
+            <img src="/NoteTakerApp.jpg" alt="Image of Note_Taker_App" />
+          </a>
+          <h2>Note Taker</h2>
+          <p>Description</p>
+        </div>
+        <div className="card">
+          <a href="">
+            <img src="/plant-leaf.svg" alt="SVG icon image of a plant." />
+          </a>
+          <h2>Green Thumb Guide</h2>
+          <p>Description</p>
+        </div>
+        <div className="card">
+          <a href="">
+            <img src="/WorkDayScheduler.jpg" alt="Image of a scheduler project" />
+          </a>
+          <h2>Work Day Scheduler</h2>
+          <p>Description</p>
+        </div>
+        <div className="card">
+          <a href="">
+            <img src="/ArtistLookup.jpg" alt="Image of an artist lookup project" />
+          </a>
+          <h2>Artist Lookup</h2>
+          <p>Description</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Projects;
