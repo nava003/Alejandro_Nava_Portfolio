f5a59b78322447fd5f1480033c642de74995b224
Alejandro <>
2024-03-20 06:38:24 -0500

    Cleaned up unwanted class names

--- src/components/svgComponents/css3SVG.jsx
+++ src/components/svgComponents/css3SVG.jsx
@@ -11,27 +11,27 @@ const Css3svg = () => {
       <path
         fill="#2143c9"
         d="M128 360 24 331 0 71h256l-24 260-104 29z"
-        class="color264de4 svgShape"
+        class="svgShape"
       />
       <path
         fill="#275fe2"
         d="m212 315 20-223H128v246l84-23z"
-        class="color2965f1 svgShape"
+        class="svgShape"
       />
       <path
         fill="#95daff"
         d="m54 189 3 32h71v-32H54zm-6-65 3 32h77v-32H48zm80 148-36-10-2-25H58l4 50 66 18v-33z"
-        class="colorebebeb svgShape"
+        class="svgShape"
       />
       <path
         fill="#2143c9"
         d="M60 0h39v16H77v16h22v17H60V0zm47 0h39v14h-23v3h23v32h-39V34h22v-2h-22V0zm46 0h39v14h-23v3h23v32h-39V34h23v-2h-23V0z"
-        class="color000000 svgShape"
+        class="svgShape"
       />
       <path
         fill="#fff"
         d="m202 189 6-65h-80v32h45l-3 33h-42v32h39l-3 41-36 10v33l65-18 1-6 7-84 1-8z"
-        class="colorffffff svgShape"
+        class="svgShape"
       />
     </svg>
   );
