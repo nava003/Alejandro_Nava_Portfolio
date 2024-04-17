14a9a548bd42bb8bd3722864fe45f04974b73458
Alejandro <>
2024-04-17 16:29:49 -0500

    Header.jsx is given logic for image profile, when clicked, reveals a resume link to a Google Drive document.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+import { Resumesvg } from "./svgComponents";
+
 function Header() {
+  const [flip, setFlip] = useState(true);
+
+  const handleImageFlip = () => {
+    setFlip(!flip)
+  }
+
+  const openInNewTab = url => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+
   return (
     <header id="home" className="main-containers">
       <h1>ALEJANDRO NAVA</h1>
-      <img src="/portfolio-face.jpg" alt="Photo of me, Alejandro"/>
+      <div className="card-container" onClick={handleImageFlip}>
+        <div className={`imageCard ${flip ? "showImg" : "showSvg"}`}>
+          <img src="/portfolio-face.jpg" alt="Photo of me, Alejandro"/>
+          <div onClick={() => openInNewTab('https://docs.google.com/document/d/1AY3qnQXK78fvNE0Zxf7L5-eNvafmp7HF/edit?usp=sharing&ouid=117446421694413359869&rtpof=true&sd=true')}><Resumesvg/></div>
+        </div>
+      </div>
       <h1>FULLSTACK DEVELOPER</h1>
     </header>
   )
