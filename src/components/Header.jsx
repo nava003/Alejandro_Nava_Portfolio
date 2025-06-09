faa58aebf1fbb78730aef7baced21e5d2402fdac
NavaHub <navaa003@gmail.com>
2025-06-09 08:27:43 -0500

    Uploaded new resume document to be viewed from flipping photo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,7 +20,7 @@ function Header() {
         <span>Click My Face</span>
         <div className={`imageCard ${flip ? "showImg" : "showSvg"}`}>
           <img src="/portfolio-face.jpg" alt="Photo of me, Alejandro"/>
-          <div onClick={() => openInNewTab('https://docs.google.com/document/d/1AY3qnQXK78fvNE0Zxf7L5-eNvafmp7HF/export?format=pdf')}><Resumesvg/></div>
+          <div onClick={() => openInNewTab('https://docs.google.com/document/d/1G1k_3Efuu5IDSrD5RKJ1u0Z1nJ8jsQlr/export?format=pdf')}><Resumesvg/></div>
         </div>
         <span>For My Resume!</span>
       </div>
